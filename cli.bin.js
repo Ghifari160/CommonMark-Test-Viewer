76c66387a88ad656be88ae5259f5ad1976d764dc
Ghifari Aditya <ghifari160@ghifari160.com>
2018-12-24 00:28:03 -0600

    Reorganize cli.bin.js
    The codebase for cli.bin.js should be maintainable for the foreseeable
    future with minimal reorganization efforts.
    
    - Add helpful (hopefully) comments throughout cli.bin.js
    - Move CTVShell (and the rest of the binary codebase) to outside of the
      libctv initialization try-catch block in cli.bin.js
    - Indicate the CTVShell codebase boundaries with comments in cli.bin.js

--- cli.bin.js
+++ cli.bin.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+// Switch to ES6
 "use strict";
 
 const path = require("path"),
@@ -21,6 +22,7 @@ var hostPlatform = process.platform, // Get host platform
 
 process.title = app.title;
 
+// Prints the app banner
 function banner()
 {
   console.log(app.title, "v" + app.version.join("."));
@@ -28,6 +30,7 @@ function banner()
   console.log("libctv v" + app.libVersion.join("."), "(" + hostPlatform + ")");
 }
 
+// Prints the about page upon execution of the `about` command
 function cmd_about()
 {
   console.log("");
@@ -46,6 +49,7 @@ function cmd_about()
   console.log("");
 }
 
+// Prints the help page upon execution of the `help` or `?` commands
 function cmd_help()
 {
   console.log("about\t\t\t\t\tAbout CommonMark Test Viewer.");
@@ -68,131 +72,148 @@ for(var i = 0; i < process.argv.length; i++)
     specTestsPy = process.argv[i];
 }
 
+// Attempt to initialize libctv
+var init;
 try
 {
-  var init = ctv.init(specTestsPy, path.dirname(path.dirname(specTestsPy)),
+  init = ctv.init(specTestsPy, path.dirname(path.dirname(specTestsPy)),
       process.env, hostPlatform);
-
-  banner();
-  console.log("");
-  console.log(`${init} tests loaded`);
-  console.log("+=========================================================+");
-  console.log("");
-
-  process.stdout.write("CTV> ");
-  var stdin = process.openStdin();
-  stdin.addListener("data", function(d)
-  {
-    var input = d.toString().trim().toLowerCase(),
-        inputInt = 0,
-        inputRange = [],
-        rangedMode = false;
-
-    if(input == "about")
-      cmd_about();
-    else if(input == "clear")
-      process.stdout.write("\x1Bc");
-    else if(input == "exit")
-      process.exit(0);
-    else if(input == "help" || input == "?")
-      cmd_help();
-    else if(input.substring(0, 4) == "html" || input.substring(0, 2) == "ht")
-    {
-      if(input.indexOf("-") > -1)
-        rangedMode = true;
-
-      if(rangedMode && input.substring(0, 4) == "html")
-      {
-        inputRange.push(parseInt(input.substring(4).split("-")[0]));
-        inputRange.push(parseInt(input.substring(4).split("-")[1]));
-      }
-      else if(rangedMode && input.substring(0, 2) == "ht")
-      {
-        inputRange.push(parseInt(input.substring(2).split("-")[0]));
-        inputRange.push(parseInt(input.substring(2).split("-")[1]));
-      }
-      else if(!rangedMode && input.substring(0, 4) == "html")
-        inputInt = parseInt(input.substring(4));
-      else
-        inputInt = parseInt(input.substring(2));
-
-      if(rangedMode && !isNaN(inputRange[0]) && !isNaN(inputRange[1]))
-      {
-        try
-        {
-          console.log(ctv.getHtmlRange(inputRange));
-        }
-        catch(e)
-        {
-          console.warn("Error:", e.message);
-        }
-      }
-      else if(!isNaN(inputInt))
-      {
-        try
-        {
-          console.log(ctv.getHtml(inputInt));
-        }
-        catch(e)
-        {
-          console.warn("Error:", e.message);
-        }
-      }
-    }
-    else if(input.substring(0, 8) == "markdown" || input.substring(0, 2) == "md")
-    {
-      if(input.indexOf("-") > -1)
-        rangedMode = true;
-
-      if(rangedMode && input.substring(0, 8) == "markdown")
-      {
-        inputRange.push(parseInt(input.substring(8).split("-")[0]));
-        inputRange.push(parseInt(input.substring(8).split("-")[1]));
-      }
-      else if(rangedMode && input.substring(0, 2) == "md")
-      {
-        inputRange.push(parseInt(input.substring(2).split("-")[0]));
-        inputRange.push(parseInt(input.substring(2).split("-")[1]));
-      }
-      else if(!rangedMode && input.substring(0, 8) == "markdown")
-        inputInt = parseInt(input.substring(8));
-      else
-        inputInt = parseInt(input.substring(2));
-
-      if(rangedMode && !isNaN(inputRange[0]) && !isNaN(inputRange[1]))
-      {
-        try
-        {
-          console.log(ctv.getMarkdownRange(inputRange));
-        }
-        catch(e)
-        {
-          console.warn("Error:", e.message);
-        }
-      }
-      else if(!isNaN(inputInt))
-      {
-        try
-        {
-          console.log(ctv.getMarkdown(inputRange));
-        }
-        catch(e)
-        {
-          console.warn("Error:", e.message);
-        }
-      }
-    }
-    else if(input.length > 0)
-    {
-      console.log("Invalid command. Type \"help\" or \"?\"",
-          "to see a list of valid commands.");
-    }
-
-    process.stdout.write("CTV> ");
-  });
 }
 catch(e)
 {
   console.warn(e.message);
   process.exit(1);
 }
+
+// Print the banner and indicate the number of tests that are loaded
+banner();
+console.log("");
+console.log(`${init} tests loaded`);
+console.log("+=========================================================+");
+console.log("");
+
+// @begin   CTVShell
+process.stdout.write("CTV> ");
+var stdin = process.openStdin();
+stdin.addListener("data", function(d)
+{
+  var input = d.toString().trim().toLowerCase(),
+      inputInt = 0,
+      inputRange = [],
+      rangedMode = false;
+
+  if(input == "about")
+    cmd_about();
+  else if(input == "clear")
+    process.stdout.write("\x1Bc");
+  else if(input == "exit")
+    process.exit(0);
+  else if(input == "help" || input == "?")
+    cmd_help();
+  else if(input.substring(0, 4) == "html" || input.substring(0, 2) == "ht")
+  {
+    // Detect if the input is a range
+    if(input.indexOf("-") > -1)
+      rangedMode = true;
+
+    // Parse range boundaries
+    if(rangedMode && input.substring(0, 4) == "html")
+    {
+      inputRange.push(parseInt(input.substring(4).split("-")[0]));
+      inputRange.push(parseInt(input.substring(4).split("-")[1]));
+    }
+    else if(rangedMode && input.substring(0, 2) == "ht")
+    {
+      inputRange.push(parseInt(input.substring(2).split("-")[0]));
+      inputRange.push(parseInt(input.substring(2).split("-")[1]));
+    }
+    // Parse input index
+    else if(!rangedMode && input.substring(0, 4) == "html")
+      inputInt = parseInt(input.substring(4));
+    else
+      inputInt = parseInt(input.substring(2));
+
+    if(rangedMode && !isNaN(inputRange[0]) && !isNaN(inputRange[1]))
+    {
+      // Attempt to print the tests in the specified range
+      try
+      {
+        console.log(ctv.getHtmlRange(inputRange));
+      }
+      catch(e)
+      {
+        console.warn("Error:", e.message);
+      }
+    }
+    else if(!isNaN(inputInt))
+    {
+      // Attempt to print the test with the specified index
+      try
+      {
+        console.log(ctv.getHtml(inputInt));
+      }
+      catch(e)
+      {
+        console.warn("Error:", e.message);
+      }
+    }
+  }
+  else if(input.substring(0, 8) == "markdown" || input.substring(0, 2) == "md")
+  {
+    // Detect if the input is a range
+    if(input.indexOf("-") > -1)
+      rangedMode = true;
+
+    // Parse range boundaries
+    if(rangedMode && input.substring(0, 8) == "markdown")
+    {
+      inputRange.push(parseInt(input.substring(8).split("-")[0]));
+      inputRange.push(parseInt(input.substring(8).split("-")[1]));
+    }
+    else if(rangedMode && input.substring(0, 2) == "md")
+    {
+      inputRange.push(parseInt(input.substring(2).split("-")[0]));
+      inputRange.push(parseInt(input.substring(2).split("-")[1]));
+    }
+    // Parse input index
+    else if(!rangedMode && input.substring(0, 8) == "markdown")
+      inputInt = parseInt(input.substring(8));
+    else
+      inputInt = parseInt(input.substring(2));
+
+    if(rangedMode && !isNaN(inputRange[0]) && !isNaN(inputRange[1]))
+    {
+      // Attempt to print the tests in the specified range
+      try
+      {
+        console.log(ctv.getMarkdownRange(inputRange));
+      }
+      catch(e)
+      {
+        console.warn("Error:", e.message);
+      }
+    }
+    else if(!isNaN(inputInt))
+    {
+      // Attempt to print the test with the specified index
+      try
+      {
+        console.log(ctv.getMarkdown(inputRange));
+      }
+      catch(e)
+      {
+        console.warn("Error:", e.message);
+      }
+    }
+  }
+  // Indicate invalid commands but ignore empty lines
+  else if(input.length > 0)
+  {
+    console.log("Invalid command. Type \"help\" or \"?\"",
+        "to see a list of valid commands.");
+  }
+
+  // Reprompt user input
+  process.stdout.write("CTV> ");
+});
+// @end   CTVShell
